767ba62f3d528113ae3caa005febf07862688e7a
chih-chien Hsiao <daniel13520cs@gmail.com>
2019-05-01 17:11:31 -0400

    all working besides distance sensors and audio shield

---
+++ i2c_intTest.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+
+#define acceAddr 0x53 //0x1D or 0x53
+
+#define DATAX0 0x32
+#define DATAX1 0x33
+#define DATAY0 0x34
+#define DATAY1 0x35
+#define DATAZ0 0x36
+#define DATAZ1 0x37
+#define Power_Register 0x2D // Power Control Register
+
+int led = 13;
+int Z0, Z1;
+
+void setup_i2c_intTest() 
+{
+  //set up high
+  pinMode(led, OUTPUT);
+  pinMode(A1, OUTPUT);
+  digitalWrite(A1, 1);
+    
+  //wiring 
+  Wire.setSDA(A4);
+  Wire.setSCL(A5);
+
+  //test
+  pinMode(A9, INPUT);  
+
+  //begin
+  Wire.begin();
+  Serial.begin(9600);
+  delay(100);
+  
+    // Enable measurement
+  Wire.beginTransmission(acceAddr);
+  Wire.write(Power_Register);
+  // Bit D3 High for measuring enable (0000 1000)
+  Wire.write(8);  
+  Wire.endTransmission();
+}
+
+void i2c_intTest() 
+{
+ //Serial.println("begin...");
+ 
+  Wire.beginTransmission(acceAddr);
+  Wire.write(DATAZ0);
+  Wire.write(DATAZ1);
+  Serial.println(Wire.endTransmission());
+
+//  Serial.print("ack = ");
+//  Serial.println(ack & (0x1), DEC);
+
+  Wire.requestFrom(acceAddr, 2);
+
+  if (Wire.available() <= 2) {
+    int z0 = Wire.read();
+    int z1 = Wire.read();
+    if (z0 < 150) {
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    } else {
+      digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
+    }
+    Z0 = z0;
+    Z1 = z1;
+  }
+
+  Serial.print("Z0 = ");
+  Serial.print(Z0);
+  Serial.print("   Z1 = ");
+  Serial.println(Z1);
+  
+delay(300);
+}
---
+++ main.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  setup_i2c_intTest();
+  setup_DistanceSensor();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //i2c_intTest();
+  //run_DistanceSensor();
+}
